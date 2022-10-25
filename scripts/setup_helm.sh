8e1b4254e66d5ad12755a285ba9d93fe346cb44d
Leo Lu <z@linkyee.com>
2022-10-25 20:17:33 +0800

    fix helm

--- scripts/setup_helm.sh
+++ scripts/setup_helm.sh
@@ -5,7 +5,7 @@ apt-get -y update
 
 #install Docker
 apt-get -y install apt-transport-https ca-certificates curl gnupg lsb-release build-essential
-curl -fsSL http://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL http://mirrors.ustc.edu.cn/docker-ce/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] http://mirrors.ustc.edu.cn/docker-ce/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get -y update
 apt-get -y install docker-ce docker-ce-cli containerd.io
