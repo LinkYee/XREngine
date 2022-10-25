beee7c1d510a4f82c284f40b86157bc301c04d62
Leo Lu <z@linkyee.com>
2022-10-25 18:13:52 +0800

    setup helm

--- scripts/setup_helm_builder.sh
+++ scripts/setup_helm_builder.sh
@@ -4,11 +4,11 @@ set -x
 sudo apt-get -y update
 
 #install kubectl
-curl -LO "http://xr-resources.yee.link/Asset/kubectl"
+curl -LO "https://dl.k8s.io/release/v1.23.6/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
 #install Helm
-curl -fsSL -o get_helm.sh http://xr-resources.yee.link/Asset/get-helm-3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
