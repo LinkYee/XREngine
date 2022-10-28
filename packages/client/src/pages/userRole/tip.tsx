bf3abf7843eececc3bc3a1d83fd0b27d4de62857
liangsongqing <2295965261@qq.com>
2022-10-28 22:30:15 +0800

    fix:新增我的奖品调用

--- packages/client/src/pages/userRole/tip.tsx
+++ packages/client/src/pages/userRole/tip.tsx
@@ -19,18 +19,18 @@ const RoleTipPage: React.FC<IntProps> = (props) => {
         props.changeSub()
     }
     return <div className="role-tip-container" >
-<div className="role-tip-content">
-        <img className="bg4Style" src={bg4} alt="" style={{ height: '100%' }} />
-        <div className='role-tip-box'>
-            <div className='role-tip-title'>
-                提示
+        <div className="role-tip-content">
+            <img className="bg4Style" src={bg4} alt="" style={{ height: '100%' }} />
+            <div className='role-tip-box'>
+                <div className='role-tip-title'>
+                    提示
+                </div>
+                <div className='role-tip-text'>
+                    欢迎进入碧桂园服务第二届客户体验创新节活动，快来选择您的虚拟人形象吧！
+                </div>
+                <div className='role-submit-btn' onClick={submit}>确定</div>
             </div>
-            <div className='role-tip-text'>
-                欢迎进入碧桂园服务创新节活动， 快来选择您的虚拟人形象吧!
-            </div>
-            <div className='role-submit-btn' onClick={submit}>确定</div>
         </div>
-</div>
     </div>
 }
 
